4ca7fe5383635f7d2f7bdc1696cffcbfe24449ac
BeatriceWambui <wachirabeatice2020@gmail.com>
2019-11-20 13:14:19 +0300

    create a portfolio

---
+++ script.js
@@ -0,0 +1,65 @@
+$(document).ready(function(){
+    $('#icon01').hover(function(){
+   
+     $(this).removeClass('icons');
+
+      },
+      function(){
+     
+     $(this).addClass('icons');
+      }); 
+
+      $('#icon02').hover(function(){
+   
+        $(this).removeClass('icons');
+   
+         },
+         function(){
+      
+        $(this).addClass('icons');
+         });
+         
+
+      $('#icon03').hover(function(){
+  
+        $(this).removeClass('icons');
+   
+         },
+         function(){
+      
+        $(this).addClass('icons');
+         });
+
+
+      $('#icon04').hover(function(){
+  
+        $(this).removeClass('icons');
+   
+         },
+         function(){
+     
+        $(this).addClass('icons');
+         });
+
+      $('#icon05').hover(function(){
+ 
+        $(this).removeClass('icons');
+   
+         },
+         function(){
+
+        $(this).addClass('icons');
+         });
+
+         $('#proj01').hover(function(){
+ 
+            $(this).removeClass('projim');
+       
+             },
+             function(){
+    
+            $(this).addClass('projim');
+             });
+  
+      
+});
